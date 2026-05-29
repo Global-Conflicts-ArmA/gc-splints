dbaecdee66779eecef3fb1ea14e7a6bbea351f27
Til <67155539+Til-Weimann@users.noreply.github.com>
2026-05-30 00:03:52 +0200

    Update GC_CharacterFracturedCondition.c

--- scripts/Game/UI/HUD/AvailableActions/Conditions/Game/Character/GC_CharacterFracturedCondition.c
+++ scripts/Game/UI/HUD/AvailableActions/Conditions/Game/Character/GC_CharacterFracturedCondition.c
@@ -3,11 +3,8 @@ class GC_CharacterFracturedCondition : SCR_AvailableActionCondition
 {
 	//------------------------------------------------------------------------------------------------
 	//! Returns true if character is bleeding for certain time
-	override bool IsAvailable(SCR_AvailableActionsConditionData data)
+	override bool IsAvailable(notnull SCR_AvailableActionsConditionData data)
 	{
-		if (!data)
-			return false;
-
 		return GetReturnResult(data.GetIsCharacterFractured());
 	}
 }
